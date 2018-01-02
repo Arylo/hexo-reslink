555d9a6242fab132c2254f5bed921c4850d2db45
Arylo <arylo.open@gmail.com>
2018-01-03 07:47:24 +0800

    Add transform script and modify deploy from `.travis.yml`

--- lib/transform.ts
+++ lib/transform.ts
@@ -1,13 +1,42 @@
 import npm = require("./npm");
 import github = require("./github");
+import { isArray } from "util";
 const parse = require("markdown-to-ast").parse;
+
+interface IBlock {
+    type: string;
+    raw: string;
+    children?: IBlock[];
+    range: [number, number];
+    loc: {
+        start: { line: number, column: number },
+        end: { line: number, column: number }
+    };
+}
 
 export = (content: string) => {
 
-    const ast = parse(content);
+    const ast: IBlock = parse(content);
+    let newContent = "";
 
-    content = npm.process(content);
-    content = github.process(content);
+    const tranChildren = (block: IBlock) => {
+        let text = block.raw;
+        if (block.children && isArray(block.children)) {
+            for (let b of block.children) {
+                tranChildren(b);
+            }
+            return;
+        }
+        if (block.type === "CodeBlock" || block.type === "Code") {
+            newContent += text;
+            return;
+        }
+        text = npm.process(text);
+        text = github.process(text);
+        newContent += text;
+    };
 
-    return content;
+    tranChildren(ast);
+
+    return newContent;
 };
--- test/transform.spec.ts
+++ test/transform.spec.ts
@@ -3,9 +3,19 @@ import transform = require("../lib/transform");
 describe('Transform Process', () => {
 
     it('Default', () => {
-        const raw = "The npm:hexo is a very nice framework. Its homepage is github:hexojs/hexo."
-        const result = `The [hexo](https://www.npmjs.com/package/hexo "Node Library: hexo") is a very nice framework. Its homepage is [hexojs/hexo](https://github.com/hexojs/hexo "Github Resource: hexojs/hexo").`
+        const raw = `The npm:hexo is a very nice framework. Its homepage is github:hexojs/hexo.`;
+        const result = `The [hexo](https://www.npmjs.com/package/hexo "Node Library: hexo") is a very nice framework. Its homepage is [hexojs/hexo](https://github.com/hexojs/hexo "Github Resource: hexojs/hexo").`;
         transform(raw).should.be.equal(result);
+    });
+
+    it("Skip Code Block", () => {
+        const raw = "```\nThe npm:hexo is a very nice framework. Its homepage is github:hexojs/hexo.\n```";
+        transform(raw).should.be.equal(raw);
+    });
+
+    it("Skip Code Block", () => {
+        const raw = "`github:hexojs/hexo-cli`";
+        transform(raw).should.be.equal(raw);
     });
 
 });
